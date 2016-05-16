43df10cc3cad1a04ac47d63c6118a1d742ead2e8
jobcrackerbah <erik@critiqueit.com>
2016-05-15 23:49:57 -0700

    adds a second ball

--- bricks.js
+++ bricks.js
@@ -1,18 +1,36 @@
 $(document).ready(function(){
-    // store reference to canvas element
+    
+    // fun variables
+    var numberBalls;
+
+    // store reference to canvas element on html page
     var canvas = document.getElementById("myCanvas");
-    // stores 2d rendering
+    // 2d rendering
     var ctx = canvas.getContext("2d");
-    // define where ball starts
+    // define where ball starts. canvas width:480px, height:320px
     var x = canvas.width/2;
     var y = canvas.height-30;
-    // change position of ball by incrementing
-    var dx = 2;
-    var dy = -2;
+    // speed of the ball
+    var drawX = -2;
+    var drawY = -2;
+
+    var x1 = canvas.width/2;
+    var y2 = canvas.height-30;
+    // speed of the ball
+    var drawX1 = 2;
+    var drawY2 = 2;
+    // ball raidus
+    var ballRadius = 10;
 
     function drawBall() {
         ctx.beginPath();
-        ctx.arc(x, y, 10, 0, Math.PI*2);
+        ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+        ctx.fillStyle = "#0095DD";
+        ctx.fill();
+        ctx.closePath();
+
+        ctx.beginPath();
+        ctx.arc(x1, y2, ballRadius, 0, Math.PI*2);
         ctx.fillStyle = "#0095DD";
         ctx.fill();
         ctx.closePath();
@@ -22,52 +40,40 @@ $(document).ready(function(){
         // clears entire canvas
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         // drawing ball and movement
+
         drawBall();
-        x += dx;
-        y += dy;
+
+
+        x += drawX;
+        y += drawY;
+        x1 += drawX1;
+        y2 += drawY2;
+
+        collisions(x,y);
+        collisions1(x1,y2);
+        
     }
+
+    function collisions(x,y){
+        if(x + drawX > canvas.width - ballRadius || x + drawX < ballRadius) {
+            drawX = -drawX;
+        }
+
+        if(y + drawY > canvas.height - ballRadius || y + drawY < ballRadius) {
+            drawY = -drawY;
+        }
+    }
+
+    function collisions1(x,y){
+        if(x + drawX1 > canvas.width - ballRadius || x + drawX1 < ballRadius) {
+            drawX1 = -drawX1;
+        }
+
+        if(y + drawY2 > canvas.height - ballRadius || y + drawY2 < ballRadius) {
+            drawY2 = -drawY2;
+        }
+    }
+    // redraws with new coordinates
     setInterval(draw, 10);
 })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // rectangle
-// ctx.beginPath();
-// ctx.rect(20, 40, 50, 50);
-// ctx.fillStyle = "#FF0000";
-// ctx.fill();
-// ctx.closePath();
-
-
-// // rectangle
-// ctx.beginPath();
-// // first two args top left.  Next two args size of rect
-// ctx.rect(160, 10, 100, 40);
-// ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
-// // adds border instead of fully colored shape
-// ctx.stroke();
-// ctx.closePath();
-
-// // circle
-// ctx.beginPath();
-// // first two args x and y coordinates, then radius, start and end angle, false clockwise
-// ctx.arc(240, 160, 20, 0, Math.PI*2, false);
-// ctx.fillStyle = "green";
-// ctx.fill();
-// ctx.closePath();
